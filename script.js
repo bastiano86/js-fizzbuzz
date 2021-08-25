bc7f863127f04c993ec87c3b1fab803ab47e71d3
bastiano86 <sebastiano_lamonaca@yahoo.com>
2021-08-25 17:25:53 +0200

    integro le formule per avere i risultati fizz, buzz e fizzbuzz acconto ai multipli indicati

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+//  Scrivi un programma che stampi i numeri da 1 a 100, 
+//  ma per i multipli di 3 stampi “Fizz” al posto del numero 
+//  e per i multipli di 5 stampi "Buzz". Per i numeri 
+//  che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
+
+
 var i; 
 for (i = 0; i <= 100; i++) {
-  console.log( i); 
-}
+  
+    console.log(i);
+
+  if (i / 3 = 0) {
+      
+    console.log (i + "Fizz");
+
+  } else if ((i / 3 = 0) && (i / 5 = 0)) {
+
+    console.log (i + "FizzBuzz");
+  };
+
+};
