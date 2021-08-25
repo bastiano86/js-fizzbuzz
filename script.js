337f85624b5b8ad7038a41633ec26aa429565e6d
bastiano86 <sebastiano_lamonaca@yahoo.com>
2021-08-25 17:55:25 +0200

    Esercizio completato

--- script.js
+++ script.js
@@ -5,17 +5,29 @@
 
 
 var i; 
-for (i = 0; i <= 100; i++) {
+for (i = 1; i <= 100; i++) {
   
-    console.log(i);
+        if ((i%3 == 0) && (i%5 == 0)) {
 
-  if (i / 3 = 0) {
-      
-    console.log (i + "Fizz");
+            console.log (i + "FizzBuzz");
+        
+        } 
+        
+        else if (i%3 == 0) {
+        
+        console.log (i + "Fizz");
 
-  } else if ((i / 3 = 0) && (i / 5 = 0)) {
+        } 
+        
+        else if (i%5 == 0) {
+        
+        console.log (i + "Buzz");
 
-    console.log (i + "FizzBuzz");
-  };
+        }
+    
+    else {
+
+        console.log(i);
+    }
 
 };
