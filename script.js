8c3d75dcd4627dc816c4452e91e4f8ed579b67ca
bastiano86 <sebastiano_lamonaca@yahoo.com>
2021-08-25 17:05:33 +0200

    secondo commit

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-// Scrivi un programma che stampi i numeri da 1 a 100, 
-// ma per i multipli di 3 stampi “Fizz” al posto del numero 
-// e per i multipli di 5 stampi "Buzz".
-// Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz"
-
+let numeri
 
 for (let i = 0; i < 100; i++) {
-   numbers.push(i);
-   console.log(i);
+   console.log(numeri);
   } 
